6818a1d4982096c13f6477f969559b6f3228f4be
karl <kar1234470@gmail.com>
2023-12-21 22:28:11 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config({ path: '.env.example' });
+
+app.use(cors());
+app.use(express.json());
+console.log(process.env.MONGO_URL , "process.env.MONGODB_URL");
+// DB connection.
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("mongo connected");
+}).catch(err => {
+    console.error("mongo starting error:", err.message);
+});
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
